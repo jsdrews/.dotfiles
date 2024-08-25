56cf12f3e8223eaec62c6211a10e86f1e898acd7
John Andrews <johnandrews@macvanderlay.lan>
2024-08-25 11:13:42 -0400

    updating vim-test keybindings

--- nvim/lua/plugins/vim-test.lua
+++ nvim/lua/plugins/vim-test.lua
@@ -3,8 +3,9 @@ return {
 	dependencies = {
 		"preservim/vimux",
 	},
-	vim.keymap.set("n", "<leader>t", ":TestNearest<CR>"),
-	vim.keymap.set("n", "<leader>T", ":TestFile<CR>"),
+	-- vim.keymap.set("n", "<leader>t", ":TestNearest<CR>"),
+	vim.keymap.set("n", "<leader>T", ":TestNearest<CR>"),
+	-- vim.keymap.set("n", "<leader>T", ":TestFile<CR>"),
 	vim.keymap.set("n", "<leader>a", ":TestSuite<CR>"),
 	vim.keymap.set("n", "<leader>l", ":TestLast<CR>"),
 	vim.keymap.set("n", "<leader>g", ":TestVisit<CR>"),
