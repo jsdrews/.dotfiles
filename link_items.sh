e365388b7cb0f7ec7697518fab46ac016d5aa2a9
John Andrews <johnandrews@macvanderlay.lan>
2024-08-29 19:58:07 -0400

    Rebrand to .dotfiles

---
+++ link_items.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+ln -sf $PWD/.config/nvim ~/.config/nvim
+ln -sf $PWD/.config/tmux ~/.config/tmux
+ln -sf $PWD/.config/fish ~/.config/fish
+ln -sf $PWD/.config/alacritty ~/.config/alacritty
+
