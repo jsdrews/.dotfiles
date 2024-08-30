523ad09a0bde1f5d315002a5dfe0f5aae87a3bed
John Andrews <johnandrews@macvanderlay.lan>
2024-08-30 18:42:36 -0400

    Add zellij config

--- link_items.sh
+++ link_items.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-ln -sf $PWD/.config/nvim ~/.config/nvim
-ln -sf $PWD/.config/tmux ~/.config/tmux
-ln -sf $PWD/.config/fish ~/.config/fish
-ln -sf $PWD/.config/alacritty ~/.config/alacritty
+ln -snf $PWD/.config/nvim ~/.config/nvim
+ln -snf $PWD/.config/tmux ~/.config/tmux
+ln -snf $PWD/.config/fish ~/.config/fish
+ln -snf $PWD/.config/alacritty ~/.config/alacritty
+ln -snf $PWD/.config/zellij ~/.config/zellij
 
