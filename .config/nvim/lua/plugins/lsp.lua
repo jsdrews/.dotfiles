4335bdb0ed1662f010afbaf85e19ee28f2954a22
John Andrews <johnandrews@macvanderlay.lan>
2024-09-02 09:25:53 -0400

    comment out macbook clangd cmd path

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -162,7 +162,7 @@ return {
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			local servers = {
 				clangd = {
-					cmd = { "/Library/Developer/CommandLineTools/usr/bin/clangd" },
+					--	cmd = { "/Library/Developer/CommandLineTools/usr/bin/clangd" },
 				},
 				gopls = {},
 				pyright = {},
