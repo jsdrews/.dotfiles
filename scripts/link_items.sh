6f0230dea3f544cb8eb84cd3a72a477e1d8e20b7
John Andrews <johnandrews@macvanderlay.lan>
2024-09-07 19:58:08 -0400

    Add link target

--- scripts/link_items.sh
+++ scripts/link_items.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-ln -snf $SCRIPT_DIR/.config/nvim ~/.config/nvim
-ln -snf $SCRIPT_DIR/.config/tmux ~/.config/tmux
-ln -snf $SCRIPT_DIR/.config/tmux/tmux.conf ~/.tmux.conf
-ln -snf $SCRIPT_DIR/.config/fish ~/.config/fish
-ln -snf $SCRIPT_DIR/.config/alacritty ~/.config/alacritty
-ln -snf $SCRIPT_DIR/.config/zellij ~/.config/zellij
+# Running from this dir won't work -- taskfile runs this in one directory up
+ln -snf $PWD/.config/nvim ~/.config/nvim
+ln -snf $PWD/.config/tmux ~/.config/tmux
+ln -snf $PWD/.config/tmux/tmux.conf ~/.tmux.conf
+ln -snf $PWD/.config/fish ~/.config/fish
+ln -snf $PWD/.config/alacritty ~/.config/alacritty
+ln -snf $PWD/.config/zellij ~/.config/zellij
 
